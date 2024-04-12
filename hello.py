8268ad727c365354fc5030484e040f6db27dd358
kishhhore <kishking2004@gmail.com>
2024-04-12 12:06:26 +0530

    added hello.txt

--- hello.py
+++ hello.py
@@ -1,2 +1,28 @@
-a , b = 10,20
-print(a+b)
+import pyautogui as py
+import time
+import random
+#write pyauto gui command to mannaly type the git command touch hello.txt , git add . , git commit -m "added hello.txt" , git push ,git pull
+def start():
+    # py.hotkey('ctrl', 'alt', 't')
+    ran = str(random.randint(1,100))
+    f= f"hello{ran}.txt"
+    time.sleep(2)
+    py.write(f'touch {f}')
+    py.press('enter')
+    time.sleep(2)
+    py.write('git add .')
+    py.press('enter')
+    time.sleep(2)
+    py.write('git commit -m "added hello.txt"')
+    py.press('enter')
+    time.sleep(2)
+    py.write('git push')
+    py.press('enter')
+    time.sleep(3)
+
+    py.write(f'rm {f}')
+    py.press('enter')
+for i in range(1,10):
+    start()
+    time.sleep(1)
+    print(f"iteration {i} completed")
